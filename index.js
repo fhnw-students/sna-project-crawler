6d1e3c43b7c26f33cbefaee87d2a4743bce987da
Gabriel Brunner <gaebu.brunner@gmail.com>
2016-10-31 14:42:35 +0100

    refactored code

--- index.js
+++ index.js
@@ -8,26 +8,24 @@ var myGexf = gexf.create();
 
 
 
+function createGexfFile(){
+	var filename = 'github.gexf';
+	// As a document
+	var gephiAsJson = myGexf.document;
 
+	// As a string
+	var gephiAsXml = myGexf.serialize();
 
-// TODO export to a .gexf file
-// SEE: https://nodejs.org/api/fs.html
-// As a document
-var gephiAsJson = myGexf.document;
-
-// As a string
-var gephiAsXml = myGexf.serialize();
-
-fs.exists('github.gexf', (exists) => {
-  if (exists){
-  	fs.unlink('github.gexf', (err) => {
-	  if (err) throw err;
-	  console.log('successfully deleted github.gexf');
+	fs.exists(filename, (exists) => {
+	 	if (exists){
+	  		fs.unlink(filename, (err) => {
+		  		if (err) throw err;
+		  		console.log('successfully deleted');
+			});
+	  	}
+	  	fs.writeFile(filename, gephiAsXml, (err) => {
+			if (err) throw err;
+			console.log('It\'s saved!');
+	  	});
 	});
-  }
-  fs.writeFile('github.gexf', gephiAsXml, (err) => {
-	if (err) throw err;
-	console.log('It\'s saved!');
-  });
-});
-
+}
