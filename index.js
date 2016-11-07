e305c54322462cee03f55e61354c0b8b26624544
Gabriel Brunner <gaebu.brunner@gmail.com>
2016-11-07 14:18:55 +0100

    create gexf from users array

--- index.js
+++ index.js
@@ -45,6 +45,7 @@ getUser('hirsch88')
             });
             Promise.all(queue).then(() => {
                 logger.info('done', users);
+                createGexfFile();
             }).catch(logger.error);
         }).catch(logger.error);
     }).catch(logger.error);
@@ -70,28 +71,23 @@ getUser('hirsch88')
 
 ///////////////////////////////
 const myGexf = gexf.create({
-    defaultEdgeType: "directed",
-    model: {
-        node: [
-            {
-                id: "name",
-                type: "string",
-                title: "Author's name"
-            },
-            {
-                id: "surname",
-                type: "string",
-                title: "Author's surname"
-            }
-        ],
-        edge: [
-            {
-                id: "predicate",
-                type: "string",
-                title: "predicate"
-            }
-        ]
-    }
+	defaultEdgeType: "directed",
+	model: {
+		node: [
+			{
+				id: "fullname",
+				type: "string",
+				title: "users's fullname"
+			}
+		],
+		edge: [
+			{
+				id: "predicate",
+				type: "string",
+				title: "predicate"
+			}
+		]
+	}
 });
 ///////////////////////////////
 
@@ -162,70 +158,110 @@ function getRequestOptions(path) {
 
 function createGexfFile() {
     var filename = 'github.gexf';
+	var addedLangs = [];
 
-    myGexf.addNode({
-        id: 'n01',
-        label: 'myFirstNode',
-        attributes: {
-            name: 'John',
-            surname: 'Silver'
-        },
-        viz: {
-            color: 'rgb(255, 234, 45)'
-        }
-    });
+	users.forEach((user) => {
+		myGexf.addNode({
+			id: user.login,
+			label: user.login,
+			attributes: {
+				fullname: user.name
+			},
+			viz: {
+				color: 'rgb(255, 234, 45)'
+			}
+		});
+		user.langs.forEach((lang) => {
+			if (addedLangs.indexOf(lang) < 0){
+				addedLangs.push(lang);
+				myGexf.addNode({
+					id: lang,
+					label: lang,
+					attributes: {
+						fullname: lang
+					},
+					viz: {
+						color: 'rgb(45, 234, 45)'
+					}
+				});
+			}
+			myGexf.addEdge({
+				id: user.login+lang,
+				source: user.login,
+				target: lang,
+				attributes: {
+					predicate: 'LIKES'
+				},
+				viz: {
+					thickness: 34
+				}
+			});
+		});
+	});
 
-    myGexf.addNode({
-        id: 'n02',
-        label: 'myFirstNode',
-        attributes: {
-            name: 'John',
-            surname: 'Doe'
-        },
-        viz: {
-            color: 'rgb(45, 234, 45)'
-        }
-    });
+	// myGexf.addNode({
+	// 	id: 'n01',
+	// 	label: 'myFirstNode',
+	// 	attributes: {
+	// 		name: 'John',
+	// 		surname: 'Silver'
+	// 	},
+	// 	viz: {
+	// 		color: 'rgb(255, 234, 45)'
+	// 	}
+	// });
 
-    myGexf.addEdge({
-        id: 'e01',
-        source: 'n02',
-        target: 'n01',
-        attributes: {
-            predicate: 'LIKES'
-        },
-        viz: {
-            thickness: 34
-        }
-    });
+	// myGexf.addNode({
+	// 	id: 'n02',
+	// 	label: 'myFirstNode',
+	// 	attributes: {
+	// 		name: 'John',
+	// 		surname: 'Doe'
+	// 	},
+	// 	viz: {
+	// 		color: 'rgb(45, 234, 45)'
+	// 	}
+	// });
 
-    myGexf.addEdge({
-        id: 'e01',
-        source: 'n01',
-        target: 'n02',
-        attributes: {
-            predicate: 'LIKES'
-        },
-        viz: {
-            thickness: 100
-        }
-    });
+	// myGexf.addEdge({
+	// 	id: 'e01',
+	// 	source: 'n02',
+	// 	target: 'n01',
+	// 	attributes: {
+	// 		predicate: 'LIKES'
+	// 	},
+	// 	viz: {
+	// 		thickness: 34
+	// 	}
+	// });
 
-    // As a document
-    var gephiAsJson = myGexf.document;
+	// myGexf.addEdge({
+	// 	id: 'e01',
+	// 	source: 'n01',
+	// 	target: 'n02',
+	// 	attributes: {
+	// 		predicate: 'LIKES'
+	// 	},
+	// 	viz: {
+	// 		thickness: 100
+	// 	}
+	// });
 
-    // As a string
-    var gephiAsXml = myGexf.serialize();
-    fs.exists(filename, (exists) => {
-        if (exists) {
-            fs.unlink(filename, (err) => {
-                if (err) throw err;
-                logger.info('successfully deleted');
-            });
-        }
-        fs.writeFile(filename, gephiAsXml, (err) => {
-            if (err) throw err;
-            logger.info('It\'s saved!');
-        });
-    });
+	// As a document
+	var gephiAsJson = myGexf.document;
+
+	// As a string
+	var gephiAsXml = myGexf.serialize();
+	fs.exists(filename, (exists) => {
+		if (exists) {
+			fs.unlink(filename, (err) => {
+				if (err) throw err;
+				logger.info('successfully deleted');
+			});
+		}
+		fs.writeFile(filename, gephiAsXml, (err) => {
+			if (err) throw err;
+			logger.info('It\'s saved!');
+		});
+	});
 }
