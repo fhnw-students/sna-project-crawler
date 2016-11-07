30f8031a2ac9f1d58f50b2ead2975445ba4d3a0a
Gery Hirschfeld <gery.hirschfeld@w3tec.ch>
2016-11-07 13:07:11 +0100

    adds basic gephi example

--- index.js
+++ index.js
@@ -3,13 +3,36 @@ const fs = require('fs');
 
 const gexf = require('gexf');
 const logger = require('./logger');
-const myGexf = gexf.create();
+const myGexf = gexf.create({
+	defaultEdgeType: "directed",
+	model: {
+		node: [
+			{
+				id: "name",
+				type: "string",
+				title: "Author's name"
+			},
+			{
+				id: "surname",
+				type: "string",
+				title: "Author's surname"
+			}
+		],
+		edge: [
+			{
+				id: "predicate",
+				type: "string",
+				title: "predicate"
+			}
+		]
+	}
+});
 
 logger.info('Starting crawling');
 
-getRepos((repos) => {
-	logger.info(repos.length);
-});
+// getRepos((repos) => {
+// 	logger.info("repos", repos.length);
+// });
 
 createGexfFile();
 
@@ -35,6 +58,43 @@ function getRepos(done) {
 
 function createGexfFile() {
 	var filename = 'github.gexf';
+
+	myGexf.addNode({
+		id: 'n01',
+		label: 'myFirstNode',
+		attributes: {
+			name: 'John',
+			surname: 'Silver'
+		},
+		viz: {
+			color: 'rgb(255, 234, 45)'
+		}
+	});
+
+	myGexf.addNode({
+		id: 'n02',
+		label: 'myFirstNode',
+		attributes: {
+			name: 'John',
+			surname: 'Doe'
+		},
+		viz: {
+			color: 'rgb(45, 234, 45)'
+		}
+	});
+
+	myGexf.addEdge({
+		id: 'e01',
+		source: 'n02',
+		target: 'n01',
+		attributes: {
+			predicate: 'LIKES'
+		},
+		viz: {
+			thickness: 34
+		}
+	});
+
 	// As a document
 	var gephiAsJson = myGexf.document;
 
