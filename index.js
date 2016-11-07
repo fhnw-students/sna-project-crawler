564b50ebf0df28424de715ce35e233720debeab1
Gery Hirschfeld <gery.hirschfeld@w3tec.ch>
2016-11-07 13:51:15 +0100

    adds example for user lang

--- index.js
+++ index.js
@@ -3,6 +3,72 @@ const fs = require('fs');
 
 const gexf = require('gexf');
 const logger = require('./logger');
+logger.info('Starting crawling');
+
+// getRepos((repos) => {
+// 	logger.info("repos", repos.length);
+// });
+
+// createGexfFile();
+
+// user = {
+// login
+// name
+// langs
+//}
+
+class User {
+
+	constructor(login, name) {
+		this.login = login;
+		this.name = name;
+		this.langs = [];
+	}
+
+}
+
+const users = [];
+getUser('dweber019')
+	.then((user) => {
+		let newUser = new User(user.login, user.name);
+		users.push(newUser);
+		getUsersRepos(user.login).then((repos) => {
+			let queue = [];
+			repos.forEach((repo) => {
+				queue.push(getUserRepoLang(user.login, repo.name).then((langs) => {
+					for (var key in langs) {
+						if (langs.hasOwnProperty(key) && newUser.langs.indexOf(key) < 0) {
+							newUser.langs.push(key);
+						}
+					}
+				}));
+			});
+			Promise.all(queue).then(() => {
+				logger.info('done', users);
+			});
+		});
+	});
+
+
+// getUser(name, (user) => {
+// 	let newUser = new User(user.login, user.name);
+// 	users.push(newUser);
+// getUsersRepos(user.login, (repos) => {
+// 	repos.forEach((repo) => {
+// 		getUserRepoLang(user.login, repo.name, (langs) => {
+// 			logger.info(user.login, repo.name, langs);
+// 			for (var key in langs) {
+// 				if (langs.hasOwnProperty(key) && newUser.langs.indexOf(key) < 0) {
+// 					newUser.langs.push(key);
+// 				}
+// 			}
+// 		});
+// 	});
+// });
+// });
+
+
+///////////////////////////////
 const myGexf = gexf.create({
 	defaultEdgeType: "directed",
 	model: {
@@ -27,17 +93,54 @@ const myGexf = gexf.create({
 		]
 	}
 });
-
-logger.info('Starting crawling');
-
-// getRepos((repos) => {
-// 	logger.info("repos", repos.length);
-// });
-
-createGexfFile();
-
-
 ///////////////////////////////
+
+function getUser(userLogin) {
+	return new Promise((resovle) => {
+		request(getRequestOptions('/users/' + userLogin), (error, response, body) => {
+			if (!error && response.statusCode == 200) {
+				resovle(JSON.parse(body));
+			} else {
+				reject();
+			}
+		});
+	});
+}
+
+function getUsersRepos(userLogin) {
+	return new Promise((resovle) => {
+		request(getRequestOptions('/users/' + userLogin + '/repos'), (error, response, body) => {
+			if (!error && response.statusCode == 200) {
+				resovle(JSON.parse(body));
+			} else {
+				reject();
+			}
+		});
+	});
+}
+
+///repos/hirsch88/angular-bootstrap-slider/languages
+function getUserRepoLang(userLogin, repoName) {
+	return new Promise((resovle, reject) => {
+		request(getRequestOptions('/repos/' + userLogin + '/' + repoName + '/languages'), (error, response, body) => {
+			if (!error && response.statusCode == 200) {
+				resovle(JSON.parse(body));
+			} else {
+				reject();
+			}
+		});
+	});
+}
+
+// function getRepos(done) {
+// 	return new Promise((resovle) => {
+// 		request(getRequestOptions('/repositories'), (error, response, body) => {
+// 			if (!error && response.statusCode == 200) {
+// 				done(JSON.parse(body));
+// 			}
+// 		});
+// 	});
+// }
 
 function getRequestOptions(path) {
 	return {
@@ -46,14 +149,6 @@ function getRequestOptions(path) {
 			'User-Agent': 'request'
 		}
 	}
-}
-
-function getRepos(done) {
-	request(getRequestOptions('/repositories'), (error, response, body) => {
-		if (!error && response.statusCode == 200) {
-			done(JSON.parse(body));
-		}
-	});
 }
 
 function createGexfFile() {
@@ -92,6 +187,18 @@ function createGexfFile() {
 		},
 		viz: {
 			thickness: 34
+		}
+	});
+
+	myGexf.addEdge({
+		id: 'e01',
+		source: 'n01',
+		target: 'n02',
+		attributes: {
+			predicate: 'LIKES'
+		},
+		viz: {
+			thickness: 100
 		}
 	});
 
