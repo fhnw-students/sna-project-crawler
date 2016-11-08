e2c27d550c48620fdd900d1fe9e1c731df1e8ab8
Gery Hirschfeld <gery.hirschfeld@w3tec.ch>
2016-11-08 14:21:44 +0100

    formats file

--- index.js
+++ index.js
@@ -8,38 +8,38 @@ const _ = require('lodash');
 
 prompt.start();
 prompt.get([
-    {
-        name: 'githubName',
-        description: 'Enter a github username',
-        type: 'string',
-        required: true
-    },
-    {
-        name: 'limit',
-        description: 'How many users should be crawled?',
-        type: 'number',
-        required: true
-    }
+  {
+    name: 'githubName',
+    description: 'Enter a github username',
+    type: 'string',
+    required: true
+  },
+  {
+    name: 'limit',
+    description: 'How many users should be crawled?',
+    type: 'number',
+    required: true
+  }
 ], (err, results) => {
-    if (!_.isNumber(results.limit)) {
-        logger.error('Please enter a valid number for limit');
-    } else {
-        logger.info('[Crawler] Starting for user ' + results.githubName);
-        github
-            .run(results.githubName, results.limit)
-            .then(writeUserJsonFile)
-            .then(gephy.createFile)
-            .catch(logger.error);
-    }
+  if (!_.isNumber(results.limit)) {
+    logger.error('Please enter a valid number for limit');
+  } else {
+    logger.info('[Crawler] Starting for user ' + results.githubName);
+    github
+      .run(results.githubName, results.limit)
+      .then(writeUserJsonFile)
+      .then(gephy.createFile)
+      .catch(logger.error);
+  }
 });
 
 function writeUserJsonFile(users) {
-    logger.info('[Crawler] Finished');
-    return new Promise((resolve, reject) => {
-        fs.writeFile('users.json', JSON.stringify(users), (err) => {
-            if (err) return logger.error(err);
-            logger.info('[Data] > users.json');
-            resolve(users);
-        });
+  logger.info('[Crawler] Finished');
+  return new Promise((resolve, reject) => {
+    fs.writeFile('users.json', JSON.stringify(users), (err) => {
+      if (err) return logger.error(err);
+      logger.info('[Data] > users.json');
+      resolve(users);
     });
+  });
 }
