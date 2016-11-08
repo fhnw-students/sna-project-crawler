ffb7fb1d4f0ce2047275448bebbc41272edeb76d
Gery Hirschfeld <gery.hirschfeld@w3tec.ch>
2016-11-08 10:43:16 +0100

    adds prompts to the crawler

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
+const prompt = require('prompt');
 const fs = require('fs');
 
 const logger = require('./src/logger');
 const github = require('./src/github');
 const gephy = require('./src/gephy');
+const _ = require('lodash');
 
-logger.info('[Crawler] Starting');
-github
-    .run('hirsch88', 12)
-    .then((users) => {
-        logger.info('[Crawler] Finished');
-        fs.writeFile('users.json', JSON.stringify(users), (err) => {
-            if (err) return logger.error(err);
-            logger.info('[Data] > users.json');
-        });
+prompt.start();
+prompt.get([
+    {
+        name: 'githubName',
+        description: 'Enter a github username',
+        type: 'string',
+        required: true
+    },
+    {
+        name: 'limit',
+        description: 'How many users should be crawled?',
+        type: 'number',
+        required: true
+    }
+], (err, results) => {
+    if (!_.isNumber(results.limit)) {
+        logger.error('Please enter a valid number for limit');
+    } else {
+        logger.info('[Crawler] Starting for user ' + results.githubName);
+        github
+            .run(results.githubName, results.limit)
+            .then((users) => {
+                logger.info('[Crawler] Finished');
+                fs.writeFile('users.json', JSON.stringify(users), (err) => {
+                    if (err) return logger.error(err);
+                    logger.info('[Data] > users.json');
+                });
 
-        //gephy.createFile(users);
-    })
-    .catch(logger.error);
-
+                //gephy.createFile(users);
+            })
+            .catch(logger.error);
+    }
+});
