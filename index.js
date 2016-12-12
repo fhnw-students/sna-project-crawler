d09f1edd2bab624ff5a159d0a2cbb911f3afd61d
Gery Hirschfeld <gery.hirschfeld@w3tec.ch>
2016-12-12 08:53:29 +0100

    adds gephy interface

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const _ = require('lodash');
 
 const logger = require('./src/logger');
 const github = require('./src/github/index');
+const githubData = require('./src/github/data');
 const gephy = require('./src/gephy');
 
 github
@@ -11,22 +12,5 @@ github
         'w3tecch'
     ], (data) => {
         logger.info('[Crawler] Finished');
-        // gephy.createFile(data);
+        gephy.createFile(githubData.getAll());
     });
-
-// function writeJsonFile(data) {
-//     return new Promise((resolve, reject) => {
-//         fs.writeFile(`${NAME}.json`, JSON.stringify(data), (err) => {
-//             if (err) return logger.error(err);
-//             logger.info('[Data] > ' + NAME + '.json');
-//             resolve(data);
-//         });
-//     });
-// }
-
-// const data = require('./src/github/data');
-// data.save().then((a)=>{
-//     console.log(a);
-// }).catch(err => {
-//     console.log(err);
-// });
